553cc3de1328b3c7658328587621a7be946e777b
Ryo Ota <nwtgck@nwtgck.org>
2022-09-16 01:09:37 +0900

    minimize

--- fetch-abort-and-http-connection/minimizing-web/src/main.js
+++ fetch-abort-and-http-connection/minimizing-web/src/main.js
@@ -1,9 +1,9 @@
-(async () => {
-  console.log("AbortController", AbortController);
-  const controller = new AbortController();
-  fetch("http://localhost:8181/m3", {
-    signal: controller.signal,
-  });
-  await new Promise(resolve => setTimeout(resolve, 3000));
+console.log("GET ...");
+const controller = new AbortController();
+fetch("http://localhost:3000/mypath", {
+  signal: controller.signal,
+});
+setTimeout(() => {
   controller.abort();
-})();
+  console.log("aborted!");
+}, 3000);
--- fetch-abort-and-http-connection/server.js
+++ fetch-abort-and-http-connection/server.js
@@ -1,30 +1,11 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  if (req.method === "GET" && req.url === "/") {
-    res.writeHead(200, {
-      "Content-Type": "text/html",
-    });
-    res.end(`\
-<script>
-const controller = new AbortController();
-fetch("/mypath", {
-  signal: controller.signal,
-});
-setTimeout(() => {
-  controller.abort();
-}, 3000);
-</script>
-`);
-    return;
-  }
-  console.log("requested");
+  console.log(`${req.method} ${req.url} requested`);
   req.on("close", () => {
-    console.log("on close");
-  });
-
-  req.on("end", () => {
-    console.log("on end");
+    console.log(`${req.url} closed`);
   });
 });
-server.listen(3000);
+server.listen(3000, (x) => {
+  console.log(`listening on ${server.address().port}`);
+});

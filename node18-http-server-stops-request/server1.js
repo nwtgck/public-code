6f512437b526b342e88d46645be3cb1afcfd6bfc
Ryo Ota <nwtgck@nwtgck.org>
2023-04-04 08:02:32 +0900

    simplify

--- node18-http-server-stops-request/server1.js
+++ node18-http-server-stops-request/server1.js
@@ -1,18 +1,17 @@
 const http = require("http");
 
-const server = http.createServer(async (req, res) => {
+const server = http.createServer((req, res) => {
   console.log(`${req.method} ${req.url}`);
+  req.on("data", (data) => console.log(`on data: ${data.length}B`));
   req.on("close", () => console.log("req closed"));
-  req.on("end", () => console.log("req end"));
   req.on("aborted", () => console.log("req aborted"));
   req.on("error", (err) => console.log("req error", err));
+  req.on("end", () => {
+    console.log("req end");
+    res.end("Finished!\n");
+  });
   res.writeHead(200);
-  for (let i = 0; i < 15; i++) {
-    res.write(`${new Date()}: Handing...\n`);
-    await new Promise(resolve => setTimeout(resolve, 2000));
-  }
-  // await new Promise(resolve => setTimeout(resolve, 30 * 1000));
-  res.end("Finished!\n");
+  res.write("Handling...\n");
 });
 
 server.listen(3000, () => {

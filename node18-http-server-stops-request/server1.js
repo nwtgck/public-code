6f9b7d5e19d9f2a76a3331a31c4d9ea12be320ac
Ryo Ota <nwtgck@nwtgck.org>
2023-04-04 07:58:07 +0900

    log

--- node18-http-server-stops-request/server1.js
+++ node18-http-server-stops-request/server1.js
@@ -2,6 +2,10 @@ const http = require("http");
 
 const server = http.createServer(async (req, res) => {
   console.log(`${req.method} ${req.url}`);
+  req.on("close", () => console.log("req closed"));
+  req.on("end", () => console.log("req end"));
+  req.on("aborted", () => console.log("req aborted"));
+  req.on("error", (err) => console.log("req error", err));
   res.writeHead(200);
   for (let i = 0; i < 15; i++) {
     res.write(`${new Date()}: Handing...\n`);
